3e61fdb90ff3b2e47e87df710130b5e67e641ca3
crackerFactory <lee.mander@gmail.com>
2023-01-25 15:26:04 +0000

    comments

--- connect-four/main.js
+++ connect-four/main.js
@@ -14,16 +14,21 @@ const columnArrs = [
 ];
 
 function placeToken() {
+  //button with event listener has id attribute which is used to access the corresponding column array and element
   const index = this.getAttribute("data-id");
 
   const columnArr = columnArrs[index];
   const column = columns[index];
 
+  //if there are still empty spaces in the column...
   if (columnArr[5] === null) {
+    //replace the first instance of null with a string of the current player's color
     const indexToReplace = columnArr.indexOf(null);
     columnArr[indexToReplace] = redTurn ? "red" : "yellow";
+    //swap whose turn it is
     redTurn = !redTurn;
     heading.innerText = redTurn ? "Red's Turn" : "Yellow's Turn";
+    //render changes to the page
     renderColumn(columnArr, column);
   }
 }
@@ -31,6 +36,7 @@ function placeToken() {
 function renderColumn(columnArr, columnEl) {
   let html = "";
   for (let i = 5; i >= 0; i--) {
+    //if the current index is truthy then set the contents as the color variable or if not set the variable to the page's background color
     const color = columnArr[i] ? columnArr[i] : "#fafafa";
     html += `
         <div class="game__space">
